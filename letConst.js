d27da78c961ad687d76d8b029510289acfed89ca
Claudia Telespan <claudiatelespan@gmail.com>
2024-10-08 17:29:53 +0300

    variables

---
+++ letConst.js
@@ -0,0 +1,14 @@
+// const test = 3;
+// test = '3';
+
+// console.log(test);
+
+let obj = {}
+
+obj.name = 'Adrian'
+
+console.log(obj.name);
+
+obj = {...obj, name:'Mihai'}
+
+console.log(obj.name);
---
+++ var.js
@@ -0,0 +1,18 @@
+function test(){
+    if(true){
+        var v1 = 1;
+        let l2 = 2;
+        const c3 = 3;
+    }
+
+    console.log(v1);
+    console.log(l2);
+    console.log(c3);
+}
+
+//test();
+
+console.log(v2);
+
+const v2 = 2;
+
